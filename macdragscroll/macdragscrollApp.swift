486eadd0ccec5a7e53c6e3ed8678cdd86ac07903
Martin Calander <martin.calander@gmail.com>
2026-01-16 19:03:53 +0100

    Init

---
+++ macdragscroll/macdragscrollApp.swift
@@ -0,0 +1,17 @@
+//
+//  macdragscrollApp.swift
+//  macdragscroll
+//
+//  Created by Martin Calander on 2026-01-16.
+//
+
+import SwiftUI
+
+@main
+struct macdragscrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
